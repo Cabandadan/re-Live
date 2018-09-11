0b089d5d101431a8cbd04864dddc2b9a725bc2bc
Cabandadan <Cabandadan@users.noreply.github.com>
2018-09-10 23:36:07 -0500

    some tests

--- scripts/main.js
+++ scripts/main.js
@@ -1,170 +1,42 @@
-/*
-Most of the stuff in here is just bootstrapping. Essentially it's just
-setting ThreeJS up so that it renders a flat surface upon which to draw
-the shader. The only thing to see here really is the uniforms sent to
-the shader. Apart from that all of the magic happens in the HTML view
-under the fragment shader.
-*/
+const W = 16;//overlay map size
+const H = W;//ditto
+const bb = 7.3;//blur bound;larger the value, smaller phantom(in map space)
+const advX = 0.5;//dis x per frame(in map space)
+const advY = advX * 1.1;//dis y per frame(in map space)
+const advFPS = 2;//det how fast it reveals(in frame)
 
-let container;
-let camera, scene, renderer;
-let uniforms;
+elCanvas.width = W;
+elCanvas.height = H;
+const ctx = elCanvas.getContext("2d");
+let T = H/2;
+let B = H/2;
+let L = W/2;
+let R = W/2;
+let x = R ;
+let y = B;
+let dir = 'R';
 
-let divisor = 1 / 10;
+ctx.fillStyle = "white";//=bg
+ctx.fillRect(0,0,W,H);
+ctx.globalCompositeOperation = 'destination-out';//see https://codepen.io/ycw/pen/OojbPZ
 
-let newmouse = {
-  x: 0,
-  y: 0
-};
-
-let loader=new THREE.TextureLoader();
-let texture, rtTexture, rtTexture2;
-loader.setCrossOrigin("anonymous");
-loader.load(
-  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/982762/noise.png',
-  function do_something_with_texture(tex) {
-    texture = tex;
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    texture.minFilter = THREE.LinearFilter;
-    init();
-    animate();
+(function f() {
+  // draw
+  switch (dir) {
+    case 'R': ctx.fillRect(x-1, y-1, advX, 1); x += advX; break;
+    case 'T': ctx.fillRect(x-1, y-1, 1, advY); y -= advY; break;
+    case 'L': ctx.fillRect(x-1, y-1, advX, 1); x -= advX; break;
+    case 'B': ctx.fillRect(x-1, y-1, 1, advY); y += advY; break;
   }
-);
 
-function init() {
-  container = document.getElementById( 'container' );
+  // "turn" in new dir if exceed threshold
+  // go R-> go T-> go L-> go B ... ccw
+  if (dir == 'R' && x > R) { dir = 'T'; R = x; }
+  else if (dir == 'T' && y < T) { dir = 'L'; T = y; }
+  else if (dir == 'L' && x < L) { dir = 'B'; L = x; }
+  else if (dir == 'B' && y > B) { dir = 'R'; B = y; }
 
-  camera = new THREE.Camera();
-  camera.position.z = 1;
-
-  scene = new THREE.Scene();
-
-  var geometry = new THREE.PlaneBufferGeometry( 2, 2 );
-
-  rtTexture = new THREE.WebGLRenderTarget(window.innerWidth * .2, window.innerHeight * .2);
-  rtTexture2 = new THREE.WebGLRenderTarget(window.innerWidth * .2, window.innerHeight * .2);
-
-  uniforms = {
-    u_time: { type: "f", value: 1.0 },
-    u_resolution: { type: "v2", value: new THREE.Vector2() },
-    u_noise: { type: "t", value: texture },
-    u_buffer: { type: "t", value: rtTexture.texture },
-    u_mouse: { type: "v2", value: new THREE.Vector2() },
-    u_renderpass: { type: 'b', value: false }
-  };
-
-  var material = new THREE.ShaderMaterial( {
-    uniforms: uniforms,
-    vertexShader: document.getElementById( 'vertexShader' ).textContent,
-    fragmentShader: document.getElementById( 'fragmentShader' ).textContent
-  } );
-  material.extensions.derivatives = true;
-
-  var mesh = new THREE.Mesh( geometry, material );
-  scene.add( mesh );
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.setPixelRatio( window.devicePixelRatio );
-
-  container.appendChild( renderer.domElement );
-
-  onWindowResize();
-  window.addEventListener( 'resize', onWindowResize, false );
-
-
-  document.addEventListener('pointermove', (e)=> {
-    let ratio = window.innerHeight / window.innerWidth;
-    newmouse.x = (e.pageX - window.innerWidth / 2) / window.innerWidth / ratio;
-    newmouse.y = (e.pageY - window.innerHeight / 2) / window.innerHeight * -1;
-
-    e.preventDefault();
-  });
-}
-
-function onWindowResize( event ) {
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  uniforms.u_resolution.value.x = renderer.domElement.width;
-  uniforms.u_resolution.value.y = renderer.domElement.height;
-
-  rtTexture = new THREE.WebGLRenderTarget(window.innerWidth * .2, window.innerHeight * .2);
-  rtTexture2 = new THREE.WebGLRenderTarget(window.innerWidth * .2, window.innerHeight * .2);
-}
-
-function animate(delta) {
-  requestAnimationFrame( animate );
-  render(delta);
-}
-
-
-
-
-
-
-let capturer = new CCapture( {
-  verbose: true,
-  framerate: 60,
-  // motionBlurFrames: 4,
-  quality: 90,
-  format: 'webm',
-  workersPath: 'js/'
- } );
-let capturing = false;
-
-isCapturing = function(val) {
-  if(val === false && window.capturing === true) {
-    capturer.stop();
-    capturer.save();
-  } else if(val === true && window.capturing === false) {
-    capturer.start();
-  }
-  capturing = val;
-}
-toggleCapture = function() {
-  isCapturing(!capturing);
-}
-
-window.addEventListener('keyup', function(e) { if(e.keyCode == 68) toggleCapture(); });
-
-let then = 0;
-function renderTexture(delta) {
-  // let ov = uniforms.u_buff.value;
-
-  let odims = uniforms.u_resolution.value.clone();
-  uniforms.u_resolution.value.x = window.innerWidth * .2;
-  uniforms.u_resolution.value.y = window.innerHeight * .2;
-
-  uniforms.u_buffer.value = rtTexture2.texture;
-
-  uniforms.u_renderpass.value = true;
-
-//   rtTexture = rtTexture2;
-//   rtTexture2 = buffer;
-
-  window.rtTexture = rtTexture;
-  renderer.setRenderTarget(rtTexture);
-  renderer.render( scene, camera, rtTexture, true );
-
-  let buffer = rtTexture
-  rtTexture = rtTexture2;
-  rtTexture2 = buffer;
-
-  // uniforms.u_buff.value = ov;
-
-  uniforms.u_buffer.value = rtTexture.texture;
-  uniforms.u_resolution.value = odims;
-  uniforms.u_renderpass.value = false;
-}
-function render(delta) {
-
-  uniforms.u_mouse.value.x += ( newmouse.x - uniforms.u_mouse.value.x ) * divisor;
-  uniforms.u_mouse.value.y += ( newmouse.y - uniforms.u_mouse.value.y ) * divisor;
-
-  uniforms.u_time.value = delta * 0.0005;
-  renderer.render( scene, camera );
-  renderTexture();
-
-  if(capturing) {
-    capturer.capture( renderer.domElement );
-  }
-}
+  // stop anim if both x and y OOB
+  if (x < W-bb && x > bb || y < H-bb && y > bb)
+    setTimeout(f, 1000/advFPS);
+}());
