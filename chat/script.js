2220dcba2222a5940bb575a9c049271011df4545
Cabandadan <caamodt@far.midco.net>
2019-06-12 11:12:55 -0500

    C

--- chat/script.js
+++ chat/script.js
@@ -69,7 +69,7 @@ function getRandomName() {
 }
 
 function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  return '#000000' //+ Math.floor(Math.random() * 0xFFFFFF).toString(16);
   // add a number each time, there is a person a go down the list per user then reset
 }
 
